27e8cbd8083e69afaf2148b5fc5876cfb3205f8d
watahbufalasheyp <danielbreiner0809@gmail.com>
2017-01-14 15:11:58 +0000

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2347,
-	"created_at": "Sat Jan 14 03:11:59 +0000 2017",
+	"tweets": 2349,
+	"created_at": "Sat Jan 14 15:11:58 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_01.js
+++ data/js/tweets/2017_01.js
@@ -1,6 +1,453 @@
 Grailbird.data.tweets_2017_01 = 
 [
 	{
+		"created_at": "Sat Jan 14 14:47:15 +0000 2017",
+		"id": 820281127306936300,
+		"id_str": "820281127306936320",
+		"text": "Good morning",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2528303382,
+			"id_str": "2528303382",
+			"name": "dan?",
+			"screen_name": "watahbufala",
+			"location": "Cork, Ireland",
+			"description": "an artist who likes to talk to much. I also like things, theyre all bad things. (he/him-♌-INTP- knight of space)",
+			"url": "https://t.co/9JqbpHksHB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9JqbpHksHB",
+							"expanded_url": "http://watahbufala.tumblr.com",
+							"display_url": "watahbufala.tumblr.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 194,
+			"friends_count": 1307,
+			"listed_count": 7,
+			"created_at": "Tue May 27 21:32:02 +0000 2014",
+			"favourites_count": 2215,
+			"utc_offset": -28800,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2346,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/782517432393031680/wH3KfXHw_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/782517432393031680/wH3KfXHw_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2528303382/1483818637",
+			"profile_link_color": "8F42F4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Sat Jan 14 14:36:29 +0000 2017",
+		"id": 820278417887297500,
+		"id_str": "820278417887297538",
+		"text": "RT @DaveMilbo: Switch size comparison with a Wii U and New 3DS XL. https://t.co/xOtEIDdes5",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DaveMilbo",
+					"name": "David Milner",
+					"id": 4328027000,
+					"id_str": "4328027000",
+					"indices": [
+						3,
+						13
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 820169808859697200,
+					"id_str": "820169808859697152",
+					"indices": [
+						67,
+						90
+					],
+					"media_url": "http://pbs.twimg.com/media/C2HUGICVEAApxBx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C2HUGICVEAApxBx.jpg",
+					"url": "https://t.co/xOtEIDdes5",
+					"display_url": "pic.twitter.com/xOtEIDdes5",
+					"expanded_url": "https://twitter.com/DaveMilbo/status/820169952548114432/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 671,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1184,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2021,
+							"h": 2048,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 820169952548114400,
+					"source_status_id_str": "820169952548114432",
+					"source_user_id": 4328027000,
+					"source_user_id_str": "4328027000"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 820169808859697200,
+					"id_str": "820169808859697152",
+					"indices": [
+						67,
+						90
+					],
+					"media_url": "http://pbs.twimg.com/media/C2HUGICVEAApxBx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C2HUGICVEAApxBx.jpg",
+					"url": "https://t.co/xOtEIDdes5",
+					"display_url": "pic.twitter.com/xOtEIDdes5",
+					"expanded_url": "https://twitter.com/DaveMilbo/status/820169952548114432/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 671,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1184,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2021,
+							"h": 2048,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 820169952548114400,
+					"source_status_id_str": "820169952548114432",
+					"source_user_id": 4328027000,
+					"source_user_id_str": "4328027000"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2528303382,
+			"id_str": "2528303382",
+			"name": "dan?",
+			"screen_name": "watahbufala",
+			"location": "Cork, Ireland",
+			"description": "an artist who likes to talk to much. I also like things, theyre all bad things. (he/him-♌-INTP- knight of space)",
+			"url": "https://t.co/9JqbpHksHB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9JqbpHksHB",
+							"expanded_url": "http://watahbufala.tumblr.com",
+							"display_url": "watahbufala.tumblr.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 194,
+			"friends_count": 1307,
+			"listed_count": 7,
+			"created_at": "Tue May 27 21:32:02 +0000 2014",
+			"favourites_count": 2215,
+			"utc_offset": -28800,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2346,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/782517432393031680/wH3KfXHw_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/782517432393031680/wH3KfXHw_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2528303382/1483818637",
+			"profile_link_color": "8F42F4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sat Jan 14 07:25:29 +0000 2017",
+			"id": 820169952548114400,
+			"id_str": "820169952548114432",
+			"text": "Switch size comparison with a Wii U and New 3DS XL. https://t.co/xOtEIDdes5",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 820169808859697200,
+						"id_str": "820169808859697152",
+						"indices": [
+							52,
+							75
+						],
+						"media_url": "http://pbs.twimg.com/media/C2HUGICVEAApxBx.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C2HUGICVEAApxBx.jpg",
+						"url": "https://t.co/xOtEIDdes5",
+						"display_url": "pic.twitter.com/xOtEIDdes5",
+						"expanded_url": "https://twitter.com/DaveMilbo/status/820169952548114432/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 671,
+								"h": 680,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1184,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2021,
+								"h": 2048,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 820169808859697200,
+						"id_str": "820169808859697152",
+						"indices": [
+							52,
+							75
+						],
+						"media_url": "http://pbs.twimg.com/media/C2HUGICVEAApxBx.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C2HUGICVEAApxBx.jpg",
+						"url": "https://t.co/xOtEIDdes5",
+						"display_url": "pic.twitter.com/xOtEIDdes5",
+						"expanded_url": "https://twitter.com/DaveMilbo/status/820169952548114432/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 671,
+								"h": 680,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1184,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2021,
+								"h": 2048,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 4328027000,
+				"id_str": "4328027000",
+				"name": "David Milner",
+				"screen_name": "DaveMilbo",
+				"location": "Melbourne, Victoria",
+				"description": "Editor of Game Informer magazine Australia. Award-winning journalist. New to Twitter. Games. Star Wars. Heavy metal. \"Consistently mildly amusing.\"",
+				"url": "https://t.co/gJH563vSHN",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/gJH563vSHN",
+								"expanded_url": "http://gameinformer.com",
+								"display_url": "gameinformer.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1046,
+				"friends_count": 597,
+				"listed_count": 13,
+				"created_at": "Sun Nov 22 23:36:40 +0000 2015",
+				"favourites_count": 17146,
+				"utc_offset": -28800,
+				"time_zone": "Pacific Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 5731,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/813201202158518272/MB1HIrwH_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/813201202158518272/MB1HIrwH_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/4328027000/1473378325",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 723,
+			"favorite_count": 590,
+			"favorited": true,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 723,
+		"favorite_count": 0,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat Jan 14 02:27:29 +0000 2017",
 		"id": 820094960007401500,
 		"id_str": "820094960007401473",
