0ef80dfb5cff1dc0e9e427e481eb24872e2c5aee
watahbufalasheyp <danielbreiner0809@gmail.com>
2017-01-24 00:12:11 +0000

    Added 7 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2912,
-	"created_at": "Mon Jan 23 23:12:09 +0000 2017",
+	"tweets": 2919,
+	"created_at": "Tue Jan 24 00:12:10 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2017_01.js",
 		"year": 2017,
 		"var_name": "tweets_2017_01",
-		"tweet_count": 1202,
+		"tweet_count": 1209,
 		"month": 1
 	},
 	{
