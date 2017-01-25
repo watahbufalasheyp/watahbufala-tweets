522aac865b2cf70edc92844e4c58ddb2cf7685f6
watahbufalasheyp <danielbreiner0809@gmail.com>
2017-01-25 01:12:07 +0000

    Added 2 tweets

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2017_01.js",
 		"year": 2017,
 		"var_name": "tweets_2017_01",
-		"tweet_count": 1236,
+		"tweet_count": 1238,
 		"month": 1
 	},
 	{
