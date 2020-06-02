fd9b5f18a57e0ff81d96ee19216cdf0660594168
watahbufalasheyp <danielbreiner0809@gmail.com>
2020-06-02 02:21:11 +0100

    Added 5 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 25501,
-	"created_at": "Mon Jun 01 01:21:11 +0000 2020",
+	"tweets": 25506,
+	"created_at": "Tue Jun 02 01:21:10 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_06.js
@@ -0,0 +1,676 @@
+Grailbird.data.tweets_2020_06 = 
+[
+	{
+		"created_at": "Mon Jun 01 18:41:36 +0000 2020",
+		"id": 1267526728462807000,
+		"id_str": "1267526728462807042",
+		"full_text": "Bruh been playing tetris the last day like a lot",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			48
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2528303382,
+			"id_str": "2528303382",
+			"name": "DꕔN🌱🌲",
+			"screen_name": "watahbufala",
+			"location": "Cork, Ireland",
+			"description": "✖️18 |/he/him ✖️music enthusiast, dumb nerd✖️ I like music, nature, and a load of nerd stuff (homestuck)✖️ INFP, maid of heart, prospit, ♌ 🧙‍♂️",
+			"url": "https://t.co/WQlPbtvFQr",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/WQlPbtvFQr",
+							"expanded_url": "https://imnotedgyiswear.carrd.co",
+							"display_url": "imnotedgyiswear.carrd.co",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 270,
+			"friends_count": 580,
+			"listed_count": 10,
+			"created_at": "Tue May 27 21:32:02 +0000 2014",
+			"favourites_count": 20506,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 23830,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1163319949424771072/1AuMwFVx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1163319949424771072/1AuMwFVx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2528303382/1559595753",
+			"profile_link_color": "036016",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "Bruh been playing tetris the last day like a lot"
+	},
+	{
+		"created_at": "Mon Jun 01 17:51:12 +0000 2020",
+		"id": 1266893729237000200,
+		"id_str": "1267514044954574851",
+		"full_text": "NORMALIZE CHANGING YOUR OPINION ON SOMETHING AFTER LEARNING NEW INFORMATION. ITS OKAY. I PROMISE.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			97
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1003842651567816700,
+			"id_str": "1003842651567816711",
+			"name": "miss toler",
+			"screen_name": "CH1ONESO",
+			"location": "Tampa, Fl",
+			"description": "rooting for everybody black. #lgbtqlivesmatter | ig chionesoo $chionesobie",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4134,
+			"friends_count": 539,
+			"listed_count": 8,
+			"created_at": "Tue Jun 05 03:35:13 +0000 2018",
+			"favourites_count": 90408,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 11052,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1266192325379854336/jTQjkTpi_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1266192325379854336/jTQjkTpi_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1003842651567816711/1583695041",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 536384,
+		"favorite_count": 1800656,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "NORMALIZE CHANGING YOUR OPINION ON SOMETHING AFTER LEARNING NEW INFORMATION. ITS OKAY. I PROMISE."
+	},
+	{
+		"created_at": "Mon Jun 01 15:18:18 +0000 2020",
+		"id": 1267475566934057000,
+		"id_str": "1267475566934056962",
+		"full_text": "Truly a gameboy camera moment https://t.co/78Kq7eCToB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			29
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1267475562530037800,
+					"id_str": "1267475562530037760",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/EZb6TwoXYAAPODX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZb6TwoXYAAPODX.jpg",
+					"url": "https://t.co/78Kq7eCToB",
+					"display_url": "pic.twitter.com/78Kq7eCToB",
+					"expanded_url": "https://twitter.com/watahbufala/status/1267475566934056962/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 797,
+							"h": 712,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 607,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 797,
+							"h": 712,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1267475562530037800,
+					"id_str": "1267475562530037760",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/EZb6TwoXYAAPODX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZb6TwoXYAAPODX.jpg",
+					"url": "https://t.co/78Kq7eCToB",
+					"display_url": "pic.twitter.com/78Kq7eCToB",
+					"expanded_url": "https://twitter.com/watahbufala/status/1267475566934056962/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 797,
+							"h": 712,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 607,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 797,
+							"h": 712,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2528303382,
+			"id_str": "2528303382",
+			"name": "DꕔN🌱🌲",
+			"screen_name": "watahbufala",
+			"location": "Cork, Ireland",
+			"description": "✖️18 |/he/him ✖️music enthusiast, dumb nerd✖️ I like music, nature, and a load of nerd stuff (homestuck)✖️ INFP, maid of heart, prospit, ♌ 🧙‍♂️",
+			"url": "https://t.co/WQlPbtvFQr",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/WQlPbtvFQr",
+							"expanded_url": "https://imnotedgyiswear.carrd.co",
+							"display_url": "imnotedgyiswear.carrd.co",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 270,
+			"friends_count": 580,
+			"listed_count": 10,
+			"created_at": "Tue May 27 21:32:02 +0000 2014",
+			"favourites_count": 20506,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 23830,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1163319949424771072/1AuMwFVx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1163319949424771072/1AuMwFVx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2528303382/1559595753",
+			"profile_link_color": "036016",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Truly a gameboy camera moment https://t.co/78Kq7eCToB"
+	},
+	{
+		"created_at": "Mon Jun 01 10:14:34 +0000 2020",
+		"id": 1267200070190940200,
+		"id_str": "1267399131238195201",
+		"full_text": "select your character https://t.co/jxb6UuQteA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			21
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1267199940981166000,
+					"id_str": "1267199940981166080",
+					"indices": [
+						22,
+						45
+					],
+					"media_url": "http://pbs.twimg.com/media/EZX_oeWUEAAZEqo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZX_oeWUEAAZEqo.jpg",
+					"url": "https://t.co/jxb6UuQteA",
+					"display_url": "pic.twitter.com/jxb6UuQteA",
+					"expanded_url": "https://twitter.com/seaminglygood/status/1267200070190940160/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1380,
+							"h": 1021,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 888,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 503,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1267199940981166000,
+					"id_str": "1267199940981166080",
+					"indices": [
+						22,
+						45
+					],
+					"media_url": "http://pbs.twimg.com/media/EZX_oeWUEAAZEqo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZX_oeWUEAAZEqo.jpg",
+					"url": "https://t.co/jxb6UuQteA",
+					"display_url": "pic.twitter.com/jxb6UuQteA",
+					"expanded_url": "https://twitter.com/seaminglygood/status/1267200070190940160/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1380,
+							"h": 1021,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 888,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 503,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 763929770602094600,
+			"id_str": "763929770602094592",
+			"name": "Jo",
+			"screen_name": "seaminglygood",
+			"location": "bad taste aquarium",
+			"description": "sonic trash",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1883,
+			"friends_count": 127,
+			"listed_count": 10,
+			"created_at": "Fri Aug 12 02:47:05 +0000 2016",
+			"favourites_count": 5122,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 452,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1261357265384169472/Slvu6DHJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1261357265384169472/Slvu6DHJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/763929770602094592/1590715068",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2310,
+		"favorite_count": 9037,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "select your character https://t.co/jxb6UuQteA"
+	},
+	{
+		"created_at": "Mon Jun 01 10:11:14 +0000 2020",
+		"id": 1267091248134918100,
+		"id_str": "1267398289550344195",
+		"full_text": "Corporations realizing they now have to pretend to care about Black Lives AND LBGT at the same time for a full month https://t.co/cit5pSlG42",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			116
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1267091244225790000,
+					"id_str": "1267091244225789953",
+					"indices": [
+						117,
+						140
+					],
+					"media_url": "http://pbs.twimg.com/media/EZWcxfYWAAEroRQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZWcxfYWAAEroRQ.jpg",
+					"url": "https://t.co/cit5pSlG42",
+					"display_url": "pic.twitter.com/cit5pSlG42",
+					"expanded_url": "https://twitter.com/DakDread/status/1267091248134918153/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 828,
+							"h": 553,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 828,
+							"h": 553,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1267091244225790000,
+					"id_str": "1267091244225789953",
+					"indices": [
+						117,
+						140
+					],
+					"media_url": "http://pbs.twimg.com/media/EZWcxfYWAAEroRQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZWcxfYWAAEroRQ.jpg",
+					"url": "https://t.co/cit5pSlG42",
+					"display_url": "pic.twitter.com/cit5pSlG42",
+					"expanded_url": "https://twitter.com/DakDread/status/1267091248134918153/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 828,
+							"h": 553,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 828,
+							"h": 553,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 936425497877246000,
+			"id_str": "936425497877245955",
+			"name": "Dredged❌",
+			"screen_name": "DakDread",
+			"location": "🏳️‍🌈 he/him",
+			"description": "Dakota • Bi •Monster fiction buff •Manga/Comic enthusiast •🔞",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2723,
+			"friends_count": 883,
+			"listed_count": 14,
+			"created_at": "Fri Dec 01 02:43:31 +0000 2017",
+			"favourites_count": 39422,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 124018,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1233010747657068545/iTo9GQyj_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1233010747657068545/iTo9GQyj_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/936425497877245955/1587849606",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 26311,
+		"favorite_count": 83953,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Corporations realizing they now have to pretend to care about Black Lives AND LBGT at the same time for a full month https://t.co/cit5pSlG42"
+	}
+]
